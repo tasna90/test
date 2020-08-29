fd2b7f6621ddc90a5aab5651beb0bc43031d45a8
tasna90 <tasnadigyula90@gmail.com>
2020-08-29 17:45:58 +0200

    refactor

--- android/src/com/monkeywars/game/states/play/PlayState.java
+++ android/src/com/monkeywars/game/states/play/PlayState.java
@@ -12,17 +12,24 @@ public class PlayState extends State {
     private Dino dino;
     private Dino dino2;
     private Texture background;
-    private int sourceX;
 
     public PlayState(GameStateManager gamneStateManager) {
         super(gamneStateManager);
+
+        //our character
         dino = new Dino(20, 110, false);
+        dino.setDinoState(Dino.DinoState.IDLE);
+
+        //opponents
         dino2 = new Dino(Gdx.graphics.getHeight(), 110, true);
         dino2.setDinoState(Dino.DinoState.OPPONENT);
+
+        //background
         background = new Texture("backgrounds/play_state_background.png");
         background.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+
+        //camera
         camera.setToOrtho(false, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-        sourceX = 0;
     }
 
     @Override
@@ -43,12 +50,10 @@ public class PlayState extends State {
     public void render(SpriteBatch spriteBatch) {
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
-        //spriteBatch.draw(background, 0,0, sourceX, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        spriteBatch.draw(background, 0, -360, sourceX, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        spriteBatch.draw(background, camera.position.x - (camera.viewportWidth / 2), 0);
         spriteBatch.draw(dino.getTexture(), dino.getPosition().x, dino.getPosition().y, Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
         spriteBatch.draw(dino2.getTexture(), dino2.getPosition().x, dino2.getPosition().y, Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
         spriteBatch.end();
-        sourceX += 5;
     }
 
     @Override
